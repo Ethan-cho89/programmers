b53246f04acc44de99bc5df8b7c38afd0d0ddad6
Donghyun_cho <bjaguar89@gmail.com>
2021-03-08 21:11:39 +0900

    noK 문제 해결

--- src/hash/PhoneNumber.java
+++ src/hash/PhoneNumber.java
@@ -2,21 +2,21 @@ package hash;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-
 public class PhoneNumber {
 	public static void main(String[] args) {
-		String[] phone_book = {"123", "456", "789", "010","78","45"};
+		String[] phone_book = {"123", "456", "789", "010","787","453"};
 		boolean answer = true;
         Arrays.sort(phone_book);
-	    HashMap<String, String> map = new HashMap<String, String>();
-	    
-	    for( String p : phone_book) {
-	    	map.put(p, p);
-	    }
-	    for(String p:phone_book) {
-	    	map.get(p);
-	    }       
+        
+        HashMap<String, String> map =  new HashMap<String, String>();
+        for(String a : phone_book) {
+        	map.put(a, a);
+        	System.out.println(a);
+        }
+        System.out.println("it's = "+map.keySet().iterator().next());
+        if(map.keySet().iterator().next()!=null) {
+        	answer=false;
+        }       
         
 		System.out.println(answer);
 	}
